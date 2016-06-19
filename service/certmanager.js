b0b590002f0b4efcc94d93926912a415e3553f9a
Chiradip Mandal <chiradip@chiradip.com>
2016-06-19 01:54:43 -0700

    topic was not formatted

--- service/certmanager.js
+++ service/certmanager.js
@@ -52,7 +52,7 @@ var certcmdtmpl2 = 'openssl ca -batch -config ../pki/etc/tls-ca.conf -in <(echo 
 var gencert = function(profile, callback) {
     if(profile.csr != null) {
         var certcmd2 = format(certcmdtmpl2, profile);
-        execsync(certcmd2, puts);
+        execsync('bash', [certcmd2], puts);
         var certpath = format('pki/certs/{nodeid}.crt', profile);
         var cert = fs.readFileSync(certpath);
         console.log('Certificate generated: ', cert);
